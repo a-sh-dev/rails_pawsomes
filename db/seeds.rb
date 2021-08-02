3dae5b55b62ddeb29e55d3b433b71b71e5410de9
a-sh <iam.angie@ymail.com>
2021-08-02 17:31:29 +1000

    heroku initial deploy success, add user to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+if User.count == 0
+  User.create(username:"Tester0", email: "test@test.com", password: "123456", password_confirmation: "123456" )
+end
