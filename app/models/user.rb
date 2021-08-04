b11c7a193ccdd129131f1553da41cb05dd796e65
“a-sh-dev” <iam.angie@ymail.com>
2021-08-04 23:03:20 +1000

    user model validations: username, bio

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :username, presence:true
+  validates :username, presence: true, length: { in: 3..12 }, format: { with: /\A[a-zA-Z0-9_]+\z/, message: "no spaces or symbols except underscores are allowed" }, uniqueness: { case_sensitive: false } 
+
+  validates :bio, length: { maximum: 100, too_long: "Only %{count} max characters allowed" }, allow_blank: true
 
 end
+
