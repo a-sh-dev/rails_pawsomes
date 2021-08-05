2e2485222c204cd84dd4add1326acbe2590a2322
“a-sh-dev” <iam.angie@ymail.com>
2021-08-05 18:10:50 +1000

    add location model for user

---
+++ app/models/location.rb
@@ -0,0 +1,6 @@
+class Location < ApplicationRecord
+  belongs_to :user
+
+  
+
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,13 @@
 class User < ApplicationRecord
+  # Model relationships
+  has_one :location
+  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  # User details validations
   validates :username, presence: true, length: { in: 3..12 }, format: { with: /\A[a-zA-Z0-9_]+\z/, message: "no spaces or symbols except underscores are allowed" }, uniqueness: { case_sensitive: false } 
 
   validates :bio, length: { maximum: 100, too_long: "Only %{count} max characters allowed" }, allow_blank: true
