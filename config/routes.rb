0f3cad0dc22b89db5d65059686e0b476d0a872b7
“a-sh-dev” <iam.angie@ymail.com>
2021-08-05 16:28:39 +1000

    configure pages routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   
   root to: "pages#home"
-  # pages
-  get 'pages/home'
+  
+  # GET static pages
+  get 'about', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
+  get 'account', to: 'pages#account'
+
 end
