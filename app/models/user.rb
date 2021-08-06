87c3a79cfbddde4007112ac5c09bf265d28e2d29
a-sh <iam.angie@ymail.com>
2021-08-06 18:23:16 +1000

    nested_attributes for registration with new devise controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
   # Model relationships
-  has_one :location
-  
+  has_one :location, dependent: :destroy
+  accepts_nested_attributes_for :location
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
