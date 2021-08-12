d52f1f12aaa911d749bf03e5ee7c502c5e8de47b
a-sh <iam.angie@ymail.com>
2021-08-12 19:07:42 +1000

    Attempt to assign user role upon registration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_12_051051) do
+ActiveRecord::Schema.define(version: 2021_08_12_064629) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 2021_08_12_051051) do
     t.string "paypal_me"
     t.string "bio"
     t.bigint "location_id", null: false
+    t.integer "role", limit: 2
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["location_id"], name: "index_users_on_location_id"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
