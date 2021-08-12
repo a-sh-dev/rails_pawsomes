28bb884cf6de057f3b63fc5b51f216d71e961101
a-sh <iam.angie@ymail.com>
2021-08-12 23:33:41 +1000

    Fix validations on location and views

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   # User details validations
-  validates :username, presence: true, length: { in: 3..12 }, format: { with: /\A[a-zA-Z0-9_]+\z/, message: "no spaces or symbols except underscores are allowed" }, uniqueness: { case_sensitive: false } 
+  validates :username, presence: true, length: { in: 3..20 }, format: { with: /\A[a-zA-Z0-9_]+\z/, message: "no spaces or symbols except underscores are allowed" }, uniqueness: { case_sensitive: false } 
 
   validates :bio, length: { maximum: 100, too_long: "Only %{count} max characters allowed" }, allow_blank: true
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -15,7 +15,35 @@ categories = [
 
 # Generate seeds
 if User.count == 0
-  User.create(username:"Bobo", email: "test0@test.com", password: "123456", password_confirmation: "123456" )
+  pawlice = User.new(
+    username: "Pawlice",
+    email: "pawlice_admin@admin-test.com.au",
+    password: "paw123456",
+    password_confirmation: "paw123456",
+    role: 0,
+    pet_owner: true,
+    paypal_me: "pawlice_admin@admin-test.com.au"
+    bio: "I pawtrol the Pawsomes community"
+  )
+  pawlice.location = Location.new(
+    country: "Australia", state: "Victoria", city: "Melbourne"
+  )
+  pawlice.save
+
+  u_owner = User.new(
+    username: "dididodo_fan",
+    email: "pawlice_admin@admin-test.com.au",
+    password: "paw123456",
+    password_confirmation: "paw123456",
+    role: 0,
+    pet_owner: true,
+    paypal_me: "pawlice_admin@admin-test.com.au"
+    bio: "I pawtrol the Pawsomes community"
+  )
+  u_owner.location = Location.new(
+    country: "Australia", state: "Victoria", city: "Melbourne"
+  )
+  u_owner.save
 end
 
 if Category.count == 0
