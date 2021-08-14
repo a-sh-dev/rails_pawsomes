5310cbc2734e05cdc1b3a7859acd1abb2d0d9aa6
a-sh <iam.angie@ymail.com>
2021-08-14 16:35:25 +1000

    Set redirections based on User's role

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -62,9 +62,17 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # end
 
   # The path used after sign up.
-  # def after_sign_up_path_for(resource)
-  #   super(resource)
-  # end
+  def after_sign_up_path_for(resource)
+    # super(resource)
+    
+    # redirect Owner to Add pets
+    if resource.role == "owner"
+      new_pet_path
+    else
+      # redirect the rest to pets index 
+      pets_path
+    end
+  end
 
   # The path used after sign up for inactive accounts.
   # def after_inactive_sign_up_path_for(resource)
--- app/models/location.rb
+++ app/models/location.rb
@@ -5,6 +5,8 @@ class Location < ApplicationRecord
   validates :country, :city, presence: true, format: { with: /\A[a-zA-Z][a-zA-Z ]+\z/, message: "alphabets only are allowed" } 
 
   # Callout methods
+  # before_save :use_existing_country
+  # before_save :use_existing_city
   before_save :lowercase_location_inputs
   before_save :remove_whitespace
 
@@ -24,7 +26,22 @@ class Location < ApplicationRecord
       self.city = self.city.strip
     end
   
+    # ! Not working yet, ideally to check existing record, if new, save
+    # def use_existing_country
+    #   if self.country.exists?
+    #     return self.country
+    #   else
+    #     curent_user.params([:location][:country])
+    #   end
+    # end
 
-
+    # def use_existing_city
+    #   if self.city.exists?
+    #     return self.city
+    #   else
+    #     curent_user.params([:location][:city])
+    #   end
+    # end
+    
 
 end
