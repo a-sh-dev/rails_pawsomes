eb5f9801ceca11d67c356a01049271ec5063e4b9
“a-sh-dev” <iam.angie@ymail.com>
2021-08-16 16:56:56 +1000

    Attempt to fix duplicate breed records for a new pet, fail

--- app/models/breed.rb
+++ app/models/breed.rb
@@ -1,17 +1,24 @@
 class Breed < ApplicationRecord
   belongs_to :category
-  has_many :pets
+  has_many :pets, dependent: :destroy 
   accepts_nested_attributes_for :category
 
   # validates :name, uniqueness: { scope: :category_id }
-  before_save :existing_breed_record
+  # before_save :existing_breed_record
+
+  private
+
+    #! TO FIX: currently duplicates record, not using existing one 
+    # def existing_breed_record
+    #   # self.id = Breed.find_by(category_id: category_id, name: name) if !Breed.find_by(category_id: category_id, name: name).nil?
+
+    #   if self.find_by(category_id, name).exists?
+    #     return self.id
+    #   end
+    # end
 
 
-  def existing_breed_record
-    if self.where(category_id && name).exists?
-      return self.id
-    end
-  end
+    
   
 
 end
