498b11ddbf068267b008b5f4a1056ab1706b51a2
a-sh <iam.angie@ymail.com>
2021-08-19 17:36:38 +1000

    Configure Pages controllers and stylise views

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,7 +1,3 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
-
-  # Load supported_pets if suppoting in "my_pets"
-  scope :that_are_supported, -> (supported_pets) { where pet_id: supported_pets }
-
 end
--- app/models/support.rb
+++ app/models/support.rb
@@ -1,4 +1,7 @@
 class Support < ApplicationRecord
   belongs_to :supporter, foreign_key: "supporter_id", class_name: "User"
   belongs_to :supported_pet, foreign_key: "supported_pet_id", class_name: "Pet"
+
+  # # Load supported_pets if suppoting in "my_pets"
+  # scope :that_are_supported, -> (supported_pets) { where(pet_id: supported_pets) }
 end
